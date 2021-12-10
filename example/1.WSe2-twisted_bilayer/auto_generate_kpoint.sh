584ab2369ec7b28a27192a0eaeae0dca778c58c7
Hyun-Jung Kim <h.kim@fz-juelich.de>
2021-12-10 16:26:57 +0100

    Update auto_generate_kpoint.sh

--- example/1.WSe2-twisted_bilayer/auto_generate_kpoint.sh
+++ example/1.WSe2-twisted_bilayer/auto_generate_kpoint.sh
@@ -5,4 +5,6 @@ WAVECAR="./WAVECAR"  # path to the wavecar is located
 NPROC=1
 #EXEC="../../vaspbaum.mpi"  # path to the "vaspbaum" program
 EXEC="/Users/Infant/code/bin/vaspbaum.mpi"  # path to the "vaspbaum" program
-mpirun -np $NPROC $EXEC -path $DIR -set_unfold -no_reduce
+
+mpirun -np $NPROC $EXEC -path $DIR -set_unfold   # generated K-points with duplicated K-points are removed
+#mpirun -np $NPROC $EXEC -path $DIR -set_unfold -no_reduce  # generated K-points with duplicated K-points are not removed
